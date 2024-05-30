068c38665bb0ce2c8eb60dd95e8f2ab3d216c775
MuhamadRezaa <mhdrezaa095@gmail.com>
2024-05-30 10:12:06 +0700

    Pesan commit Anda

--- src/hello.php
+++ src/hello.php
@@ -4,7 +4,7 @@ namespace mrza\labhello;
 
 class hello {
     public function tampilHello($name) {
-        return "Hello $name, selamat datang di library sederhana";
+        return "Hello $name, selamat datang di kelas TRPL 2D";
     }
 }
 
